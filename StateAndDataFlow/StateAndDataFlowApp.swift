0fc45671c92b790e0ff42bed5375504f286c89a4
alexsokolov <frank9154@gmail.com>
2024-03-09 23:42:32 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,24 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    @StateObject private var contentViewVM = ContentViewViewModel()
+    @StateObject private var loginViewVM = LoginViewViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+        .environmentObject(contentViewVM)
+        .environmentObject(loginViewVM)
+    }
+      
+    
+}
